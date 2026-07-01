e93e1bed52bc814ebf635a687fe2d3c588413162
anshikakalhans <anshikaakalhans@gmail.com>
2026-07-01 22:32:46 +0530

    Initial commit

---
+++ src/Basics.java
@@ -0,0 +1,7 @@
+void main(){
+    System.out.println("hello world" + " hello duniya");
+    System.out.println(3);
+    System.out.println("1+2");
+    // Below there is an expression.
+    System.out.println(1+2);
+}
