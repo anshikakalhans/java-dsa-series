b6453c3167729dbb5671c4e0c62292eb784be6b5
anshikakalhans <anshikaakalhans@gmail.com>
2026-07-02 11:52:45 +0530

    practice 01

---
+++ src/Practice_01.java
@@ -0,0 +1,29 @@
+//Try creating variables for:
+//
+//Your contest rating
+//Total problems solved
+//Platform subscription status
+//Your name
+
+
+
+
+public class Practice_01 {
+    public static void main(String[] args) {
+        // Contest Rating
+        int contestRating = 1500;
+        // Total Problems Solved
+        int totalProblemsSolved = 200;
+        // Platform Subscription Status
+        boolean isSubscribed = true;
+        // Name
+        String name = "Anshika Kalhans";
+
+        // Displaying the values
+        System.out.println("Contest Rating: " + contestRating);
+        System.out.println("Total Problems Solved: " + totalProblemsSolved);
+        System.out.println("Subscribed: " + isSubscribed);
+        System.out.println("Name: " + name);
+    }
+}
+
