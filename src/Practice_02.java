514a64cdb34258945f407e8b49e272c3e43739ee
anshikakalhans <anshikaakalhans@gmail.com>
2026-07-02 21:20:33 +0530

    Practice 02

---
+++ src/Practice_02.java
@@ -0,0 +1,31 @@
+//Try creating variables for:
+//
+//Number of contests attended
+//Average problem-solving time
+//Whether premium member or not
+//Grade in mock test
+
+
+
+
+public class Practice_02 {
+    public static void main(String[] args) {
+
+        // Number of contests attended
+        int contestsAttended = 500;
+        System.out.println("Contests Attended: " + contestsAttended);
+
+        // Average Problem-Solving Time ( in minutes )
+        double avgProblemSolvingTime = 10.5;
+        System.out.println("Average Problem Solving Time: " + avgProblemSolvingTime + " min");
+
+        // Premium Member or Not
+        boolean isPremiumMember = false;
+        System.out.println("Premium Membership: " + isPremiumMember);
+
+        // Grade in Mock Test
+        char grade = 'A';
+        System.out.println("Grade in Mock Test: " + grade);
+
+    }
+}
