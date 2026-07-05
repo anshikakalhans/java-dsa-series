a3ff327824d6478438e1c1939a988fcaf19a38c0
anshikakalhans <anshikaakalhans@gmail.com>
2026-07-05 22:35:55 +0530

    Practice 04

---
+++ src/Practice_04.java
@@ -0,0 +1,77 @@
+// Q1. Take age input and print if he/she eligible to vote or not.
+
+//public static void main(String[] args) {
+//
+//    System.out.println("Enter your age: ");
+//
+//    Scanner sc = new Scanner(System.in);
+//    int age = sc.nextInt();
+//
+//    if (age >= 18) {
+//        System.out.println("You can vote.");
+//    } else {
+//        System.out.println("You cannot vote.");
+//
+//    }
+//}
+
+
+// Q2. Take input 5 subject's marks and print the overall percentage of student.
+
+//void main() {
+//
+//    Scanner sc = new Scanner(System.in);
+//
+//    System.out.println("Enter your marks for Python: ");
+//    float marksOfPython = sc.nextFloat();
+//
+//    System.out.println("Enter your marks for Cpp ");
+//    float marksOfCpp = sc.nextFloat();
+//
+//    System.out.println("Enter your marks for Java ");
+//    float marksOfJava = sc.nextFloat();
+//
+//    System.out.println("Enter your marks for JS ");
+//    float marksOfJS = sc.nextFloat();
+//
+//    System.out.println("Enter your marks for React ");
+//    float marksOfReact = sc.nextFloat();
+//
+//    float total = marksOfPython + marksOfCpp + marksOfJava + marksOfJS + marksOfReact;
+//    float percentage = (total / 500) * 100;
+//
+//    System.out.println("Your Overall Percentage is:" + percentage);
+//
+//    sc.close();
+//
+//}
+
+
+// Q3. Take input 5 subject's marks, drop the least one and calculate the overall percentage considering only the top 4 marks, print it.
+
+void main() {
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Enter your marks for Python: ");
+    float marksOfPython = sc.nextFloat();
+
+    System.out.println("Enter your marks for Cpp ");
+    float marksOfCpp = sc.nextFloat();
+
+    System.out.println("Enter your marks for Java ");
+    float marksOfJava = sc.nextFloat();
+
+    System.out.println("Enter your marks for JS ");
+    float marksOfJS = sc.nextFloat();
+
+    System.out.println("Enter your marks for React ");
+    float marksOfReact = sc.nextFloat();
+
+}
+
+
+
+
+
+
