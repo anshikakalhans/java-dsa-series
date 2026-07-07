10945e7e594ce0790b7c8e20e67fcd7705cd6038
anshikakalhans <anshikaakalhans@gmail.com>
2026-07-07 16:58:29 +0530

    for loop

---
+++ src/Loops.java
@@ -0,0 +1,54 @@
+public class Loops {
+    static void main() {
+
+        // for loop
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println(i);
+//        }
+
+        // another example
+//        for (int i = 1; i <= 5; i++) {
+//            System.out.println("Anshika Kalhans");
+//        }
+
+        // another example
+//        for (int i = 1; i <= 10; i += 2) {
+//            System.out.println(i);
+//        }
+
+        // nested loop
+//        for (int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+        // another example of nested loop
+//        for (int i = 1; i <= 3; i++) {
+//            for (int j = 1; j <= 3; j++) {
+//                System.out.println("i = " + i + ", j = " + j);
+//            }
+//        }
+
+        // break keyword code ---> current loop se bahar nikal deta hai
+//        for (int i = 1; i <= 10; i++) {
+//            if (i == 5) {
+//                break;
+//            }
+//            System.out.println(i);
+//        }
+
+        // continue keyword code ---> skip the iteration
+//        for (int i = 1; i <= 10; i++) {
+//            if (i == 6 || i == 8) {
+//                continue;
+//            }
+//            System.out.println(i);
+//        }
+
+
+    }
+}
+
+
