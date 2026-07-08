d12e9255c17c60b33334f0df5984392265d7c468
anshikakalhans <anshikaakalhans@gmail.com>
2026-07-08 22:30:54 +0530

    Practice 07

---
+++ src/Practice_07.java
@@ -0,0 +1,126 @@
+// 1. Print numbers from 1 to 10 using for loop.
+
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i);
+//        }
+//    }
+//}
+
+// 2. Print numbers from 10 to 1 using while loop.
+
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        int i = 10;
+//        while (i >= 1) {
+//            System.out.println(i);
+//            i--;
+//        }
+//    }
+//}
+
+// 3. Print first 5 even numbers using for loop.
+
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        for (int i = 1; i <= 10; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+//}
+
+// 4. Print multiplication table of 7.
+
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println("7 * " + i + " = " + (7 * i));
+//
+//        }
+//    }
+//}
+
+// 5. Print sum of numbers from 1 to n.
+
+//import java.util.Scanner;
+//
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter n: ");
+//        int n = sc.nextInt();
+//
+//        int sum = 0;
+//
+//        for (int i = 1; i <= n; i++) {
+//            sum = sum + i;
+//        }
+//
+//        System.out.println("Sum is: " + sum);
+//    }
+//}
+
+// 6. Print only odd numbers from 1 to 20 using continue.
+
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        for (int i = 1; i <= 20; i++) {
+//            if (i % 2 == 0) {
+//                continue;
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+//}
+
+// 7. Stop loop when value becomes 6 using break.
+
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        for (int i = 1; i <= 10; i++) {
+//            if (i == 6) {
+//                break;
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+//    }
+//}
+//
+
+// 8. Print square pattern of size n using nested loops.
+
+//import java.util.Scanner;
+//
+//public class Practice_07 {
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Enter n: ");
+//        int n = sc.nextInt();
+//
+//        for (int i = 1; i <= n; i++) {
+//            for (int j = 1; j <= n; j++) {
+//                System.out.print("*");
+//            }
+//
+//            System.out.println();
+//
+//        }
+//    }
+//}
+
+
