66c2a74f05882c31b910efdf361598b883ac6e00
anshikakalhans <anshikaakalhans@gmail.com>
2026-08-20 20:30:15 +0530

    methods

--- src/Methods.java
+++ src/Methods.java
@@ -1,62 +1,62 @@
-public class Methods {
-
-    // declaration/definition
-    static void print2KaTable() {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(2 * i);
-        }
-
-    }
-
-    static void printSum(int x, int y) { // parameter declaration
-        System.out.println("Sum: " + (x + y));
-    }
-
-    static void multiply(int a, int b) {
-        int ans = a * b;
-        System.out.println("Result: " + ans);
-        return;
-    }
-
-    // non void return type
-
-    static int add(int p, int q) {
-        int sum = p + q;
-        return sum;
-    }
-
-    static float add(int p, float q) {
-        float ans = p + q;
-        return ans;
-    }
-
-    static void main() {
-
-//        System.out.println("hie");
+//public class Methods {
 //
-//        // method call/invoke
-//        print2KaTable();
+//    // declaration/definition
+//    static void print2KaTable() {
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(2 * i);
+//        }
 //
-//        System.out.println("bye");
+//    }
+//
+//    static void printSum(int x, int y) { // parameter declaration
+//        System.out.println("Sum: " + (x + y));
+//    }
+//
+//    static void multiply(int a, int b) {
+//        int ans = a * b;
+//        System.out.println("Result: " + ans);
+//        return;
+//    }
+//
+//    // non void return type
+//
+//    static int add(int p, int q) {
+//        int sum = p + q;
+//        return sum;
+//    }
+//
+//    static float add(int p, float q) {
+//        float ans = p + q;
+//        return ans;
+//    }
+//
+//    static void main() {
+//
+////        System.out.println("hie");
+////
+////        // method call/invoke
+////        print2KaTable();
+////
+////        System.out.println("bye");
+//
+//        //printSum(5, 10); // argument declaration
+//
+//        //multiply(2, 3);
+//
+//        //int result = add(2, 2);
+//        //System.out.println("Result: " + result);
+//
+//        int ans1 = add(2, 2);
+//        float ans2 = add(2, 2.0f);
+//
+//        System.out.println("Ans1 is: " + ans1);
+//        System.out.println("Ans2 is: " + ans2);
+//
+//    }
+//}
 
-        //printSum(5, 10); // argument declaration
-
-        //multiply(2, 3);
-
-        //int result = add(2, 2);
-        //System.out.println("Result: " + result);
-
-        int ans1 = add(2, 2);
-        float ans2 = add(2, 2.0f);
-
-        System.out.println("Ans1 is: " + ans1);
-        System.out.println("Ans2 is: " + ans2);
-
-    }
-}
 
 
-
-
+// Methods and Variable Scoping
 
 
