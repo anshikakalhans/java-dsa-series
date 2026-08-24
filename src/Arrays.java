85cc43dc2b9472eb6d4fab4720c7efcc42795eb2
anshikakalhans <anshikaakalhans@gmail.com>
2026-08-24 11:42:34 +0530

    completed 1D Arrays

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,23 +1,129 @@
-public class Arrays {
-    static void main() {
+// ---------- 1D Arrays ----------
 
-        // declaration
-        int arr[];
-        // allocation
-        arr = new int[5];
-        // initialization
-        int brr[] = {1, 2, 3};
+// import java.util.Scanner;
+
+//public class Arrays {
+//    static void main() {
+//
+//        // declaration
+//        int arr[];
+//        // allocation
+//        arr = new int[5];
+//        // initialization
+//        int brr[] = {1, 2, 3};
 
 
-        // accessing elements of array using for loop
-        int n = brr.length;
-        for (int index = 0; index <= n - 1; index++) {
-            System.out.println(brr[index]);
-        }
+// accessing elements of array using for loop
+//        int n = brr.length;
+//        for (int index = 0; index <= n - 1; index++) {
+//            System.out.println(brr[index]);
+//        }
 
-        // accessing elements of array
-        System.out.println("Value at index 0: " + brr[0]);
-        System.out.println("Value at index 1: " + brr[1]);
-        System.out.println("Value at index 2: " + brr[2]);
-    }
-}
+// accessing elements of array
+//        System.out.println("Value at index 0: " + brr[0]);
+//        System.out.println("Value at index 1: " + brr[1]);
+//        System.out.println("Value at index 2: " + brr[2]);
+
+// using for each loop
+//        for(int val: brr){
+//            System.out.println(val);
+//        }
+//   }
+
+// taking input
+//    static void main() {
+
+//        int arr[] = new int[5];
+//        Scanner sc = new Scanner(System.in);
+//        int n = arr.length;
+//
+//        for (int i = 0; i <= n - 1; i++) {
+//            System.out.println("Provide input for index " + i);
+//            arr[i] = sc.nextInt();
+//        }
+//
+//        // print
+//        System.out.println("You array contains: ");
+//        for (int val : arr) {
+//            System.out.println(val);
+//}
+
+//        int arr[] = {1, 2, 3};
+//        int n = arr.length;
+//        for (int i = 0; i <= n - 1; i++) {
+//            System.out.println(arr[i]);
+//        }
+//    }
+
+// ----- Print Sum of Array -----
+//public class Arrays {
+//    static void main() {
+//
+//        int arr[] = {10, 20, 30, 40, 50};
+//        int sum = 0;
+//        int n = arr.length;
+//
+//        for (int i = 0; i <= n - 1; i++) {
+//            int value = arr[i];
+//            sum = sum + value;
+//
+//        }
+//
+//        // sum is ready to print
+//        System.out.println(sum);
+//    }
+//}
+
+// Multiply elements of Array
+//class Arrays {
+//    static void main() {
+//
+//        int arr[] = {2, 3, 10, 20};
+//        int ans = 1;
+//        int n = arr.length;
+//        for (int i = 0; i <= n - 1; i++) {
+//            int value = arr[i];
+//            ans = ans * value;
+//        }
+//        System.out.println(ans);
+//    }
+//}
+
+// Find Maximum Element in an Array
+//class Arrays {
+//    static void main() {
+//
+//        int arr[] = {2, 4, 6, 8, 10, -76, 100, 1005, 23};
+//        int n = arr.length;
+//        int maxValue = arr[0];
+//
+//        for (int i = 0; i <= n - 1; i++) {
+//            if (arr[i] > maxValue) {
+//                // update maxValue
+//                maxValue = arr[i];
+//            }
+//        }
+//        System.out.println(maxValue);
+//    }
+//}
+
+// Find the minimum value in an Array
+//class Arrays {
+//    static void main() {
+//        int arr[] = {2, 3, 4, 5, 6, 5, 4, 3, 2, 1, 10, 0, 11};
+//        int n = arr.length;
+//        int minValue = arr[0];
+//
+//        for (int i = 0; i <= n - 1; i++) {
+//            if (arr[i] < minValue) {
+//                // update minValue
+//                minValue = arr[i];
+//            }
+//        }
+//        System.out.println(minValue);
+//    }
+//}
+
+
+
+
